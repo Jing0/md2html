f0be06102539db64f21f272939f2c7a15babd76a
Jackie Kuo <j.kuo2012@gmail.com>
2014-08-16 13:47:58 +0800

    0.2

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-#About
+# MD2HTML
 
-a C implementation of the Markdown  to HTML system.
+a C implementation of the Markdown to HTML system.
+
+## Supported
+
+* `Customized CSS`
+* `Headers` ( from # to #### )
+* `Part Separator`( --- ).
+* `Link` ( (name)[url] )
+* `Blockquote` ( > )
+
+## ToDo
+
+* `Image` ( !(name)[url] )
+* `List` ( * item )
+* `Italic Style` ( *some text* )
+* `Bold Style` ( **some text** )
+* and so and forth
--- md2html.c
+++ md2html.c
@@ -5,7 +5,7 @@
  *
  *    Description:  a C implementation of the Markdown to HTML system.
  *
- *        Version:  1.0
+ *        Version:  0.2
  *        Created:  08/15/2014 14:01:15
  *       Revision:  none
  *       Compiler:  gcc
@@ -18,7 +18,9 @@
 #include <stdio.h>
 #include <string.h>
 
+FILE *in_fp, *out_fp;
 char title[257];
+int isHr = 0;
 
 void get_dest(char *file_dest){
 	printf("Please input the directory of the target file:\n");
@@ -41,83 +43,216 @@ void new_name(char *new, char *old){
 	strcat(new, ".html");
 }
 
-void add_style(FILE *fp){
+void add_style(){
 	FILE *css_fp;
 	char ch, style_file[12] = "./style.css";
 
-	fprintf(fp, "%s", "<style>\n");
+	fprintf(out_fp, "%s", "<style>\n");
 
 	if((css_fp = fopen(style_file, "r")) == NULL){
 		printf("error! can't open style file(%s)", style_file);
 	}
+
 	while((ch = fgetc(css_fp)) != EOF){
-		fputc(ch, fp);;
+		fputc(ch, out_fp);;
 	}
-	fprintf(fp, "%s", "\n</style>\n");
+
+	fprintf(out_fp, "%s", "\n</style>\n");
 }
 
-void add_header(FILE *fp){
-	char *header1 = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">\n\n";
-	char *header2 = "<title>";
-	char *header3 = "</title>\n</head>\n<body>\n";
+void add_head(){
+	char *head1 = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">\n\n";
+	char *head2 = "<title>";
+	char *head3 = "</title>\n</head>\n<body>\n";
 
-	fprintf(fp, "%s", header1);
+	fprintf(out_fp, "%s", head1);
 
-	add_style(fp);
+	add_style();
 		
-	fprintf(fp, "%s%s%s", header2, title, header3);
+	fprintf(out_fp, "%s%s%s", head2, title, head3);
 }
 
-void add_footer(FILE *fp){
-	char *footer = "\n</body>\n</html>\n";
-	fprintf(fp, "%s", footer);
+void add_foot(){
+	char *foot = "\n</body>\n</html>\n";
+	fprintf(out_fp, "%s", foot);
 }
 
-void convert(FILE *out_fp, FILE *in_fp){
+int onHeader(){
+	int count = 1;
 	char ch;
-	int isNewLine = 0, count;
 
-	add_header(out_fp);
-	fprintf(out_fp, "<p>");
+	while((ch = fgetc(in_fp) ) == '#'){
+		++count;
+	}
+
+	if( count > 6 ){
+		while(count--){
+			fprintf(out_fp, "#");
+		}
+		fprintf(out_fp, "%c", ch);
+		return 1;
+	}
+
+	fprintf(out_fp, "<h%d>%c", count, ch);
+
+	ch = fgetc(in_fp);
+	while(ch != '\n' && ch != EOF){
+		fprintf(out_fp, "%c", ch);
+		ch = fgetc(in_fp);
+	}
+
+	fprintf(out_fp, "</h%i>\n", count);
+	return 0;
+}
+
+int onUrl(){
+	int i = 0;
+	char ch, name[1000], url[1000];
+
+	while((ch = fgetc(in_fp)) != EOF){
+		if(ch == ']' || ch == '\n' || i > 999 ){
+			break;
+		}
+		name[i++] = ch;
+	}
+	name[i] = '\0';
+
+	if( ch == '\n' || ch == EOF ){
+		fprintf(out_fp, "[%s%c", name, ch);
+		return 1;
+	}
+
+	while((ch = fgetc(in_fp) ) == ' ');
+	if( ch == '(' ){
+		i = 0;
+		while((ch = fgetc(in_fp)) != EOF){
+			if(ch == ')' || ch == '\n'){
+				break;
+			}
+			url[i++] = ch;
+		}
+		url[i] = '\0';
+
+		if(ch == '\n' || ch == EOF){
+			fprintf(out_fp, "[%s](%s%c", name, url, ch);
+			return 3;
+		}
+	}
+	else{
+		fprintf(out_fp, "[%s]%c", name, ch);
+		return 2;
+	}
+
+	fprintf(out_fp, "<a href=\"%s\">%s</a>", url, name);
+	return 0;
+}
+
+int onHr(){
+	int count = 1;
+	char ch;
+
+	ch = fgetc(in_fp);
+	while(ch != '\n' && ch == '-'){
+		++count;
+		ch = fgetc(in_fp);
+	}
+
+	if( count >= 3 ){
+		fprintf(out_fp, "\n<hr/>\n");
+		isHr = 1;
+	}
+	else{
+		while(count--){
+			fprintf(out_fp, "-");
+		}
+		fputc(ch, out_fp);
+		return 1;
+	}
+	return 0;
+}
+
+
+void convert(){
+	char ch;
+	int isNewLine, isQuote, isCode;
+	isNewLine = 1;
+	isQuote = isCode = 0;
+
+	add_head();
 	
 	while((ch = fgetc(in_fp)) != EOF){
-		if(ch == '\n'){
-			if(isNewLine){
-				continue;
+		if(isNewLine){
+			if( ch == '>' ||  ch == '#' || ch == '-' || ch == '*' ){
+				if( ch == '>' ){
+					if( isQuote == 0 ){
+						fprintf(out_fp, "<%s>\n", "blockquote" );
+						isQuote = 1;
+					}
+				}
+				else if( ch == '#'){
+					onHeader();
+					ch = '\n';   // hack: set newline to 1;
+				}
+				else if( ch == '-' ){
+					onHr();
+				}
+				/*
+				else if( ch == '*' ){
+					onList("ul");
+				}
+				*/
+				else{
+					fputc(ch, out_fp);
+				}
+			}
+			else if( isQuote == 1 && ch != '>' ){
+				fprintf(out_fp, "</blockquote>\n");
+				isQuote = 0;
 			}
 			else{
+				if(ch != '\n'){
+					fprintf(out_fp, "<p>%c", ch);
+				}
+				else{
+					fputc(ch, out_fp);
+				}
+			}
+		}
+		// This is characters cannot display
+		/*
+		else if( ch == '&' || ch == '<' ){
+			if( ch == '&' ){
+				fprintf(out_fp, "&amp;");
+			}
+			else{
+				fprintf(out_fp, "&lt;");
+			}
+		}
+		*/
+		else if( ch == '[' ){
+            onUrl();
+        }
+        else if( ch != '\n'){
+            fputc(ch, out_fp);
+        }
+
+        if( ch != '\n' ){
+            isNewLine = 0;
+        }
+        else{
+			if( !isHr && !isNewLine ){
 				fprintf(out_fp, "</p>\n");
 			}
 			isNewLine = 1;
-		}
-		else{
-			if(isNewLine){
-				isNewLine = 0;
-				if(ch == '#'){
-					count = 1;
-					while((ch = fgetc(in_fp)) == '#'){
-						++count;
-					}
-					fprintf(out_fp, "<h%d>%c", count, ch);
-					while((ch = fgetc(in_fp)) != '\n'){
-						fputc(ch, out_fp);
-					}
-					fprintf(out_fp, "</h%d>\n\n", count);
-				}
-				
-				else{
-					fprintf(out_fp, "<p>");
-				}
-			}
-			fputc(ch, out_fp);
-		}
+			isHr = 0;
+        }
+
 	}
 
-	add_footer(out_fp);
+	add_foot();
 }
 
 int main(){
-	FILE *in_fp, *out_fp;
 	char in_file[100], out_file[100];
 
 	get_dest(in_file);
@@ -130,7 +265,7 @@ int main(){
 		printf("error! can't create file(%s)", out_file);
 	}
 
-	convert(out_fp, in_fp);
+	convert();
 
 	printf("\nDone!\n");
 	fclose(in_fp);
